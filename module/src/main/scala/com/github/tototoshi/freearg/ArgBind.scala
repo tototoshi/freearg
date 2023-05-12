bd9599d36076fd908be667e7bbe10c9188f8ec26
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2023-05-12 22:02:44 +0900

    scalafmt

--- module/src/main/scala/com/github/tototoshi/freearg/ArgBind.scala
+++ module/src/main/scala/com/github/tototoshi/freearg/ArgBind.scala
@@ -12,17 +12,21 @@ object ArgBind:
   given [M[_]](using ME: MonadError[M, Throwable]): ArgBind[M, String] with
     def bind(name: String, stringValue: Option[String], defaultValue: Option[String]): M[String] =
       stringValue
-      .orElse(defaultValue)
-      .map(ME.pure)
-      .getOrElse(ME.raiseError(new ArgParserException(s"${name} is not specified")))
+        .orElse(defaultValue)
+        .map(ME.pure)
+        .getOrElse(ME.raiseError(new ArgParserException(s"${name} is not specified")))
 
   given [M[_]](using ME: MonadError[M, Throwable]): ArgBind[M, Int] with
     def bind(name: String, stringValue: Option[String], defaultValue: Option[Int]): M[Int] =
       stringValue match
         case Some(s) =>
           try { ME.pure(s.toInt) }
-          catch { case e: NumberFormatException => ME.raiseError(new ArgParserException(s"${name} could not be parsed as int")) }
-        case None => defaultValue.map(ME.pure).getOrElse(ME.raiseError(new ArgParserException(s"option is not specified: $name")))
+          catch {
+            case e: NumberFormatException =>
+              ME.raiseError(new ArgParserException(s"${name} could not be parsed as int"))
+          }
+        case None =>
+          defaultValue.map(ME.pure).getOrElse(ME.raiseError(new ArgParserException(s"option is not specified: $name")))
 
   given [M[_], T](using ME: MonadError[M, Throwable], B: ArgBind[M, T]): ArgBind[M, Option[T]] with
     def bind(name: String, stringValue: Option[String], defaultValue: Option[Option[T]]): M[Option[T]] =
